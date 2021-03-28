c3d35893ebf3a63e92ab59c584318450ad755dc7
Nellis,Dean <dean.nellis@lexmark.com>
2021-03-28 19:22:26 -0400

    Footer story and improvements

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Icon from "../Icon";
+import kamiMark from "../../images/kami_mark.png";
 
 const Footer = () => (
   <footer className="footer">
     <div className="footer__left">
-      <Icon />
+      <Link to="/">
+        <img src={kamiMark} alt="kami brand mark" height="40px" />
+      </Link>
     </div>
     <div className="footer__copyright">
-      © Dean Nellis {new Date().getFullYear()}
+      <a href="https://deannellis.me/">
+        © Dean Nellis {new Date().getFullYear()}
+      </a>
     </div>
     <div className="footer__right">
-      <Icon title="Facebook Logo" />
+      <a href="https://github.com/deannellis/kami">
+        <Icon title="GitHub Logo" />
+      </a>
     </div>
   </footer>
 );
