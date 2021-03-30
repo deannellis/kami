e0eedadcb35b6a83ec1598c08c19db491f072afd
Nellis,Dean <dean.nellis@lexmark.com>
2021-03-30 10:25:18 -0400

    Added icons

--- src/components/Icon/iconList.ts
+++ src/components/Icon/iconList.ts
@@ -1,6 +1,12 @@
 export default [
     'Moon Icon',
     'Sun Icon',
+    'WiFi Icon',
+    'PDF Icon',
+    'Stacked Dots Icon',
+    'Info Icon',
+    'Menu Icon',
     'Facebook Logo',
     'GitHub Logo',
+    'LinkedIn Logo',
 ]
