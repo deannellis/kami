bcdc0798e6a7880ece8dee11f72d5cf7691f7fb7
Nellis,Dean <dean.nellis@lexmark.com>
2021-04-08 13:22:04 -0400

    Loading animation story

---
+++ src/components/LoadingAnimation/LoadingAnimation.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LoadingAnimationComponent from "./index";
+
+export default {
+  title: "Elements/Loading Animation",
+  component: LoadingAnimationComponent,
+};
+
+export const LoadingAnimation = () => <LoadingAnimationComponent />;
