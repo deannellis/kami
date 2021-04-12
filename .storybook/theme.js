8becd2829fa5963d99258d5193f464fbe7ba39ba
Nellis,Dean <dean.nellis@lexmark.com>
2021-04-12 13:06:24 -0400

    added custom storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  brandImage:
+    "https://raw.githubusercontent.com/deannellis/kami/master/src/images/kami_logo.png",
+  brandUrl: "https://github.com/deannellis/kami",
+  base: "light",
+  brandTitle: "kami",
+});
