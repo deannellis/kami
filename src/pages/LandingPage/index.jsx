9ccc3533b446c6678c5bc81f5ee326023d3464cc
Nellis,Dean <dean.nellis@lexmark.com>
2021-04-27 11:08:18 -0400

    Technology listing section complete

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 import PageLayout from "../../components/PageLayout";
+import Icon from "../../components/Icon";
 
+const techInfo = [
+  {
+    title: "Formik",
+    description: (
+      <p>
+        Kami uses <a href="https://formik.org/">Formik</a> to create custom
+        inputs and forms. Building advanced forms with input validation is now
+        effortless.
+      </p>
+    ),
+  },
+  {
+    title: "TypeScript",
+    description: (
+      <p>
+        Get all the benefits of IntelliSense with components that are statically
+        typed with <a href="https://www.typescriptlang.org/">TypeScript</a>,
+        making them easy to read, understand, and use.
+      </p>
+    ),
+  },
+  {
+    title: "Snowpack",
+    description: (
+      <p>
+        Instant startup, lightning-fast refreshes, and hot module replacement
+        are all included with the{" "}
+        <a href="https://www.snowpack.dev/">Snowpack</a> build tool. Experience
+        the difference.
+      </p>
+    ),
+  },
+];
 const LandingPage = () => (
   <PageLayout layout="no-pad">
     <section className="landing-page__hero">
@@ -25,6 +59,14 @@ const LandingPage = () => (
         </li>
         <li>To try new technologies & best practices</li>
       </ul>
+    </section>
+    <section className="landing-page__technologies">
+      {techInfo.map(({ title, description }) => (
+        <div>
+          <Icon title={`${title} Mark`} size={80} />
+          {description}
+        </div>
+      ))}
     </section>
   </PageLayout>
 );
